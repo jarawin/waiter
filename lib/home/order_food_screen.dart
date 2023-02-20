f5c9dca3537d3bf5c981603585e6f8f3add7d628
buddy5230 <74503111+buddy5230@users.noreply.github.com>
2023-02-20 18:09:20 +0700

    iconbar and clicking icon to new page.

---
+++ lib/home/order_food_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:food_delivery/home/food_page_body.dart';
+import 'package:food_delivery/utils/colors.dart';
+import 'package:food_delivery/utils/dimensions.dart';
+import 'package:food_delivery/widgets/big_text.dart';
+import 'package:food_delivery/widgets/small_text.dart';
+
+class OrderFood extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('หน้าออเดอร์ที่สั่ง'),
+    );
+  }
+}
